54228294d12fa6810aefa10cadeb569da74dae63
Janis Pritzkau <janispritzkau@gmail.com>
2025-03-28 14:53:03 +0100

    feat: embed model viewer

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Obsidian Model Viewer
 
 This plugin allows you to view and embed interactive 3D models directly in your [Obsidian](http://obsidian.md/) vault, powered by Google’s [\<model-viewer\>](https://modelviewer.dev/) component.
+
+![Screenshot](screenshot.avif)
+
+> \<model-viewer\> supports and plans to support only glTF/GLB 3D models. It is the Khronos standard known as the JPEG of 3D and the first format to standardize Physically-Based Rendering (PBR), making your models look realistic under any lighting, on any renderer. It is also compact, compressible, and loads rapidly into the GPU.
+
+[\<model-viewer\> FAQ](https://modelviewer.dev/docs/faq.html)
+
+## Features
+
+==TODO==
--- main.ts
+++ main.ts
@@ -1,11 +1,33 @@
-import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import type { ModelViewerElement } from "@google/model-viewer";
+import {
+	App,
+	Component,
+	FileView,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	type TFile,
+	type WorkspaceLeaf,
+} from "obsidian";
+
+if (customElements.get("model-viewer") == null) {
+	import("@google/model-viewer");
+}
 
 interface ModelViewerSettings {
-	mySetting: string;
+	cameraControls: boolean;
+	disablePan: boolean;
+	disableZoom: boolean;
+	autoRotate: boolean;
+	interactionPrompt: boolean;
 }
 
 const DEFAULT_SETTINGS: ModelViewerSettings = {
-	mySetting: "default",
+	cameraControls: true,
+	disablePan: false,
+	disableZoom: false,
+	autoRotate: true,
+	interactionPrompt: true,
 };
 
 export default class ModelViewerPlugin extends Plugin {
@@ -15,10 +37,31 @@ export default class ModelViewerPlugin extends Plugin {
 		await this.loadSettings();
 
 		this.addSettingTab(new ModelViewerSettingTab(this.app, this));
+
+		this.registerView("model-viewer-file-view", (leaf) => {
+			return new ModelViewerFileView(leaf, this);
+		});
+
+		this.registerExtensions(["glb", "gltf"], "model-viewer-file-view");
+
+		// @ts-expect-error
+		this.app.embedRegistry.registerExtensions(
+			["gltf", "glb"],
+			(
+				context: {
+					containerEl: HTMLElement;
+					displayMode: boolean;
+					linktext: string;
+					showInline: boolean;
+				},
+				file: TFile
+			) => new ModelViewerComponent(this, context.containerEl, file)
+		);
 	}
 
 	onunload() {
-		//
+		// @ts-expect-error
+		this.app.embedRegistry.unregisterExtensions(["gltf", "glb"]);
 	}
 
 	async loadSettings() {
@@ -28,6 +71,97 @@ export default class ModelViewerPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+}
+
+class ModelViewerComponent extends Component {
+	plugin: ModelViewerPlugin;
+	containerEl: HTMLElement;
+	file: TFile;
+	viewer: ModelViewerElement;
+
+	constructor(plugin: ModelViewerPlugin, containerEl: HTMLElement, file: TFile) {
+		super();
+		this.plugin = plugin;
+		this.containerEl = containerEl;
+		this.file = file;
+		containerEl.addClass("model-viewer-embed");
+		this.updateWidth();
+
+		this.observer = new MutationObserver(() => {
+			this.updateWidth();
+		});
+		this.observer.observe(this.containerEl, {
+			attributes: true,
+			attributeFilter: ["width"],
+		});
+	}
+
+	onunload(): void {
+		this.observer.disconnect();
+		this.viewer?.remove();
+	}
+
+	observer: MutationObserver;
+
+	unload(): void {
+		this.observer.disconnect();
+	}
+
+	updateWidth() {
+		const width = this.containerEl.getAttribute("width");
+		this.containerEl.style.width = width ? `${width}px` : "100%";
+	}
+
+	loadFile() {
+		this.viewer = document.createElement("model-viewer");
+		this.viewer.src = this.plugin.app.vault.getResourcePath(this.file);
+		this.containerEl.appendChild(this.viewer);
+		const settings = this.plugin.settings;
+		if (settings.cameraControls) this.viewer.cameraControls = true;
+		if (settings.disablePan) this.viewer.disablePan = true;
+		if (settings.disableZoom) this.viewer.disableZoom = true;
+		if (settings.autoRotate) this.viewer.autoRotate = true;
+		if (!settings.interactionPrompt) this.viewer.interactionPrompt = "none";
+	}
+}
+
+class ModelViewerFileView extends FileView {
+	private plugin: ModelViewerPlugin;
+
+	constructor(leaf: WorkspaceLeaf, plugin: ModelViewerPlugin) {
+		super(leaf);
+		this.plugin = plugin;
+	}
+
+	getViewType(): string {
+		return "model-viewer-file-view";
+	}
+
+	canAcceptExtension(extension: string): boolean {
+		return ["glb", "gltf"].includes(extension);
+	}
+
+	async onLoadFile(file: TFile): Promise<void> {
+		this.displayInEl(file);
+	}
+
+	displayInEl(file: TFile) {
+		const viewer = document.createElement("model-viewer");
+		viewer.style.position = "absolute";
+		viewer.style.width = "100%";
+		viewer.style.height = "100%";
+		viewer.src = this.app.vault.getResourcePath(file);
+		this.contentEl.appendChild(viewer);
+
+		const settings = this.plugin.settings;
+		if (settings.cameraControls) viewer.cameraControls = true;
+		if (settings.disablePan) viewer.disablePan = true;
+		if (settings.disableZoom) viewer.disableZoom = true;
+		if (settings.autoRotate) viewer.autoRotate = true;
+		if (!settings.interactionPrompt) viewer.interactionPrompt = "none";
+	}
+
+	async onUnloadFile(file: TFile): Promise<void> {}
 }
 
 class ModelViewerSettingTab extends PluginSettingTab {
@@ -44,15 +178,61 @@ class ModelViewerSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
+			.setName("Camera Controls")
+			.setDesc("Enable camera controls for the model")
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.cameraControls).onChange(async (value) => {
+					this.plugin.settings.cameraControls = value;
+					await this.plugin.saveSettings();
+				})
+			);
+		new Setting(containerEl)
+			.setName("Disable Pan")
+			.setDesc("Disable panning the model")
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.disablePan).onChange(async (value) => {
+					this.plugin.settings.disablePan = value;
+					await this.plugin.saveSettings();
+				})
+			);
+		new Setting(containerEl)
+			.setName("Disable Zoom")
+			.setDesc("Disable zooming the model")
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.disableZoom).onChange(async (value) => {
+					this.plugin.settings.disableZoom = value;
+					await this.plugin.saveSettings();
+				})
+			);
+		new Setting(containerEl)
+			.setName("Auto Rotate")
+			.setDesc("Enable auto rotation of the model")
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.autoRotate).onChange(async (value) => {
+					this.plugin.settings.autoRotate = value;
+					await this.plugin.saveSettings();
+				})
+			);
+		new Setting(containerEl)
+			.setName("Interaction Prompt")
+			.setDesc("Show prompt to interact with the model")
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.interactionPrompt).onChange(async (value) => {
+					this.plugin.settings.interactionPrompt = value;
+					await this.plugin.saveSettings();
+				})
+			);
+		new Setting(containerEl)
+			.setName("Reset Settings")
+			.setDesc("Reset settings to default")
+			.addButton((button) =>
+				button
+					.setButtonText("Reset to default")
+					.setWarning()
+					.onClick(async () => {
+						this.plugin.settings = DEFAULT_SETTINGS;
 						await this.plugin.saveSettings();
+						this.display();
 					})
 			);
 	}
