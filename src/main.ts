b4c7b5480ee88c7630d5a25adac2053b940b1638
Janis Pritzkau <janispritzkau@gmail.com>
2025-04-01 21:10:50 +0200

    wip: code block embeds with annotation support

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { Plugin, type TFile } from "obsidian";
+import { MarkdownRenderer, parseYaml, Plugin, type TFile } from "obsidian";
+import { ModelViewerComponent, type ModelViewerComponentOptions } from "./ModelViewerComponent";
 import { ModelViewerEmbed } from "./ModelViewerEmbed";
 import { ModelViewerFileView } from "./ModelViewerFileView";
 import { ModelViewerSettingTab } from "./ModelViewerSettingTab";
@@ -45,6 +46,61 @@ export default class ModelViewerPlugin extends Plugin {
 				);
 			}
 		);
+
+		this.registerMarkdownCodeBlockProcessor("model-viewer", async (source, el, ctx) => {
+			el.classList.add("model-viewer-embed");
+
+			const { src, hotspots, overlay, aspect, height, ...attributes } = parseYaml(source);
+
+			const file = this.app.metadataCache.getFirstLinkpathDest(src, ctx.sourcePath);
+			if (file == null) {
+				el.createEl("p", { text: "File not found" });
+				return;
+			}
+
+			const options: ModelViewerComponentOptions = {
+				enableOverlay: this.settings.embed.enableOverlay,
+				aspectRatio: this.settings.embed.aspectRatio,
+				maxHeight: this.settings.embed.maxHeight,
+				attributes: {
+					...this.settings.embed.attributes,
+					...attributes,
+				},
+			};
+
+			if (overlay) {
+				options.enableOverlay = overlay;
+			}
+
+			if (aspect) {
+				options.aspectRatio = aspect;
+			}
+
+			if (height) {
+				options.height = height;
+			}
+
+			const viewer = new ModelViewerComponent(el, options);
+			ctx.addChild(viewer);
+
+			const viewerEl = viewer.viewerEl;
+			viewerEl.src = this.app.vault.getResourcePath(file);
+
+			for (const hotspot of hotspots ?? []) {
+				const el = viewerEl.createEl("div");
+				el.classList.add("hotspot");
+				el.setAttribute("slot", `hotspot-${hotspot.id}`);
+				if (hotspot.surface) {
+					el.setAttribute("data-surface", hotspot.surface);
+				} else {
+					el.setAttribute("data-position", hotspot.position);
+					el.setAttribute("data-normal", hotspot.normal);
+				}
+				el.setAttribute("data-visibility-attribute", "visible");
+				MarkdownRenderer.render(this.app, hotspot.text, el, ctx.sourcePath, this);
+				viewerEl.appendChild(el);
+			}
+		});
 	}
 
 	onunload() {
